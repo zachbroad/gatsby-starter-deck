bad063b76defc0c74ddc4e4d490932700ae77d3e
zachbroad <zachbroad@gmail.com>
2019-08-03 01:32:43 -0400

    aye aye

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    name: `Fabian Schultz`,
-    title: `Gatsby Deck`,
-    date: `July 30, 2018`,
+    name: `The Captain's Quarters`,
+    title: `Aye Aye Captain!`,
+    date: `2019`,
   },
   plugins: [
     `gatsby-plugin-layout`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { navigate } from 'gatsby';
+import captain from '../images/captain.gif';
+import './styles.css';
 
 class Index extends Component {
   componentDidMount() {
-    navigate(`/1`, { replace: true });
+    // navigate(`/1`, { replace: true });
   }
+
   render() {
-    return <div />;
+      return (
+          <div style={{flex: 1}}>
+            <img className="gif" src={captain} />
+          </div>
+      );
   }
 }
 
